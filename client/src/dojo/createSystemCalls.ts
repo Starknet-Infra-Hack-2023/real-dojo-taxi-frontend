acdadbaa6b42fcb4fd2672b28f87ed5ffa8390c1
democritus <ponderingdemocritus@protonmail.com>
2023-10-12 16:11:35 +0200

    fix pos

--- client/src/dojo/createSystemCalls.ts
+++ client/src/dojo/createSystemCalls.ts
@@ -13,9 +13,11 @@ export function createSystemCalls(
     { Position, Moves }: ClientComponents
 ) {
 
+    const { VITE_PUBLIC_WORLD_ADDRESS } = import.meta.env;
+
     const spawn = async (signer: Account) => {
 
-        const entityId = parseInt(signer.address) as EntityIndex;
+        const entityId = signer.address.toString() as EntityIndex;
 
         const positionId = uuid();
         Position.addOverride(positionId, {
@@ -26,11 +28,11 @@ export function createSystemCalls(
         const movesId = uuid();
         Moves.addOverride(movesId, {
             entity: entityId,
-            value: { remaining: 100 },
+            value: { remaining: 10 },
         });
 
         try {
-            const tx = await execute(signer, "player_actions", 'spawn', ['0x2430f23de0cd9a957e1beb7aa8ef2db2af872cc7bb3058b9be833111d5518f5']);
+            const tx = await execute(signer, "player_actions", 'spawn', [VITE_PUBLIC_WORLD_ADDRESS]);
 
             console.log(tx)
             const receipt = await signer.waitForTransaction(tx.transaction_hash, { retryInterval: 100 })
@@ -48,7 +50,11 @@ export function createSystemCalls(
 
     const move = async (signer: Account, direction: Direction) => {
 
-        const entityId = parseInt(signer.address) as EntityIndex;
+        const entityId = signer.address.toString() as EntityIndex;
+
+        console.log("direction", direction)
+        console.log("getComponentValue", getComponentValue(Position, entityId))
+
 
         const positionId = uuid();
         Position.addOverride(positionId, {
@@ -63,7 +69,7 @@ export function createSystemCalls(
         });
 
         try {
-            const tx = await execute(signer, "player_actions", "move", [direction]);
+            const tx = await execute(signer, "player_actions", "move", [VITE_PUBLIC_WORLD_ADDRESS, direction]);
 
             console.log(tx)
             const receipt = await signer.waitForTransaction(tx.transaction_hash, { retryInterval: 100 })
@@ -87,8 +93,8 @@ export function createSystemCalls(
 }
 
 export enum Direction {
-    Left = 0,
-    Right = 1,
-    Up = 2,
-    Down = 3,
+    Left = 1,
+    Right = 2,
+    Up = 3,
+    Down = 4,
 }
